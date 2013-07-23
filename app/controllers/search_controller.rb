9efbc1620800ed8a7f2b653248da3a160783b919
Muhammad <majidnaviwala@gmail.com>
2013-07-23 02:14:47 -0500

    Modified search syntax for searching
    using to_i this time and just testing for core values for now

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,13 @@ class SearchController < ApplicationController
   	# @robot_designs = RobotDesign.all
   	# @projects = Project.all
 
-  	@core_values = CoreValue.where("team_number ILIKE ? ", "%#{params[:search]}%")
-  	@robot_designs = RobotDesign.where("team_number ILIKE ? ", "%#{params[:search]}%")
-  	@projects = Project.where("team_number ILIKE ? ", "%#{params[:search]}%")
+  	# @core_values = CoreValue.where("team_number ILIKE ? ", "%#{params[:search]}%")
+  	# @robot_designs = RobotDesign.where("team_number ILIKE ? ", "%#{params[:search]}%")
+  	# @projects = Project.where("team_number ILIKE ? ", "%#{params[:search]}%")
+
+  	# @core_values = CoreValue.find(params[:search])
+  	# @core_values = CoreValue.find(:conditions => ["team_number = ?", params[:search].to_i])
+  	@core_values = CoreValue.where("team_number LIKE ?", params[:search].to_i)
 
   end
 end

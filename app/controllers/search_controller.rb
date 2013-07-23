1a6334484058e8ebd4a1b668764a72e7e5d3c51e
Muhammad <majidnaviwala@gmail.com>
2013-07-23 08:42:39 -0500

    Search will now work in production as well !
    Finally !!!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,10 @@
 class SearchController < ApplicationController
-  def search_results
-  	# @core_values = CoreValue.all
-  	# @robot_designs = RobotDesign.all
-  	# @projects = Project.all
 
-  	# @core_values = CoreValue.where("team_number ILIKE ? ", "%#{params[:search]}%")
-  	# @robot_designs = RobotDesign.where("team_number ILIKE ? ", "%#{params[:search]}%")
-  	# @projects = Project.where("team_number ILIKE ? ", "%#{params[:search]}%")
+	def search_results
+		@core_values = CoreValue.where("team_number = ?", params[:search].to_i)
+		@projects = Project.where("team_number = ?", params[:search].to_i)
+		@robot_designs = RobotDesign.where("team_number = ?", params[:search].to_i)
 
-  	# @core_values = CoreValue.find(params[:search])
-  	# @core_values = CoreValue.find(:conditions => ["team_number = ?", params[:search].to_i])
-  	@core_values = CoreValue.where("team_number = ?", params[:search].to_i)
+	end
 
-  end
 end
